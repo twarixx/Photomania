99bc9a790a79d530cf972d5c82139f5403a0ca1b
twarixx <amberful@craftville.net>
2022-11-17 13:05:03 +0100

    Added notifications button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import '../App.css';
-import {Link, useNavigate, useParams} from "react-router-dom";
-import {Menu, Transition, Dialog} from "@headlessui/react";
+import {Link, useNavigate} from "react-router-dom";
+import {Menu, Transition, Dialog, Popover} from "@headlessui/react";
 import {useState} from "react";
 import users from "./Users";
 
@@ -27,7 +27,7 @@ function Header() {
                 bar.blur();
                 closeModal();
 
-                const user = users.find(user => user.username.toLowerCase()  === searchValue.toLowerCase() || user.display_name.toLowerCase() === searchValue.toLowerCase());
+                const user = users.find(user => user.username.toLowerCase() === searchValue.toLowerCase() || user.display_name.toLowerCase() === searchValue.toLowerCase());
                 if (!user) {
                     bar.setAttribute('placeholder', 'This user could not be found!');
                     bar.style.borderColor = 'red';
@@ -51,10 +51,14 @@ function Header() {
         <>
             <nav
                 className="absolute bg-black text-white bg-opacity-30 h-14 sm:h-20 w-full flex items-center px-5 justify-between shadow-sm">
-                <div>
+                <div className="flex space-x-2 items-center">
                     <Link to="/"><h1 className="hidden sm:block font-semibold text-lg tracking-widest">PHOTOMANIA</h1>
                     </Link>
                     <Link to="/"><h1 className="block sm:hidden font-semibold text-lg tracking-widest">PM</h1></Link>
+
+                    <img onClick={openModal}
+                         className="block sm:hidden mt-1 w-6 sm:w-9 mr-3 sm:mr-5 h-full fill-white text-white"
+                         src="/icons/search-white.svg" alt="Search" title="Search"/>
                 </div>
 
                 <div className="hidden sm:block flex-grow max-w-xl">
@@ -63,11 +67,10 @@ function Header() {
 
 
                 <div className="flex items-center">
-                    <img onClick={openModal}
-                         className="block sm:hidden w-8 sm:w-9 mr-3 sm:mr-5 h-full fill-white text-white"
-                         src="/icons/search-white.svg" alt="Search" title="Search"/>
-                    <img className="w-8 sm:w-9 mr-3 sm:mr-5 h-full fill-white text-white"
-                         src="/icons/notifications.svg" alt="Search" title="Search"/>
+                    <Link to="/"><img className="w-8 sm:w-9 mr-3 sm:mr-5 h-full" src="/icons/home.svg"
+                                      alt="Home" title="Home"/></Link>
+                    {notifications()}
+
                     <Link to="/upload"><img className="w-8 sm:w-9 mr-3 sm:mr-5 h-full" src="/icons/upload.svg"
                                             alt="Upload" title="Upload"/></Link>
 
@@ -133,38 +136,73 @@ function showSearch({handleSubmit, handleChange}) {
     )
 }
 
+function notifications() {
+    return (
+        <div className="mb-[-5px] z-30">
+            <Popover className="relative">
+                {({open}) => (
+                    <>
+                        <Popover.Button>
+                            <img className="w-8 sm:w-9 mr-3 sm:mr-5 h-full" src="/icons/notifications.svg"
+                                 alt="Notifications" title="Notifications"/>
+                        </Popover.Button>
+                        <Transition
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0 translate-y-1"
+                            enterTo="opacity-100 translate-y-0"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100 translate-y-0"
+                            leaveTo="opacity-0 translate-y-1"
+                        >
+                            <Popover.Panel
+                                className="absolute left-1/2 z-30 mt-3 w-screen max-w-md -translate-x-3/4 transform px-4 sm:px-0">
+                                <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                                    <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+                                        <p className="text-black font-semibold">Notifications</p>
+                                    </div>
+                                </div>
+                            </Popover.Panel>
+                        </Transition>
+                    </>
+                )}
+            </Popover>
+        </div>
+    )
+}
+
 function userDropdown() {
     return (
         <Menu>
             <div className="flex flex-col z-30">
 
-            <Menu.Button><img
-                className="w-9 sm:w-11 aspect-square object-cover rounded-full border-2 border-solid border-white relative"
-                title="Esmay" src="/images/profile_pictures/esmay.jpg" alt="Profile Pic"/></Menu.Button>
-            <Transition
-                enter="transition duration-100 ease-out"
-                enterFrom="transform scale-95 opacity-0"
-                enterTo="transform scale-100 opacity-100"
-                leave="transition duration-75 ease-out"
-                leaveFrom="transform scale-100 opacity-100"
-                leaveTo="transform scale-95 opacity-0"
-            >
-                <Menu.Items
-                    className="absolute right-0 z-[300] w-56 rounded-md bg-gray-400 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="z-[100] px-1 py-1 ">
-                        <Menu.Item>
-                            <Link to="/esmaybe"><p className="px-4 py-2 border-b hover:bg-gray-700">Profile</p></Link>
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Link to="/settings"><p className="px-4 py-2 border-b hover:bg-gray-700">Account
-                                settings</p></Link>
-                        </Menu.Item>
-                        <Menu.Item>
-                            <Link to="/logout"><p className="px-4 py-2 hover:bg-gray-700">Log out</p></Link>
-                        </Menu.Item>
-                    </div>
-                </Menu.Items>
-            </Transition>
+                <Menu.Button><img
+                    className="w-9 sm:w-11 aspect-square object-cover rounded-full border-2 border-solid border-white relative"
+                    title="Esmay" src="/images/profile_pictures/esmay.jpg" alt="Profile Pic"/></Menu.Button>
+                <Transition
+                    enter="transition duration-100 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-75 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0"
+                >
+                    <Menu.Items
+                        className="absolute right-0 z-[300] w-56 rounded-md bg-gray-400 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="z-[100] px-1 py-1 ">
+                            <Menu.Item>
+                                <Link to="/esmaybe"><p className="px-4 py-2 border-b hover:bg-gray-700">Profile</p>
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to="/settings"><p className="px-4 py-2 border-b hover:bg-gray-700">Account
+                                    settings</p></Link>
+                            </Menu.Item>
+                            <Menu.Item>
+                                <Link to="/logout"><p className="px-4 py-2 hover:bg-gray-700">Log out</p></Link>
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Transition>
             </div>
         </Menu>
     )
