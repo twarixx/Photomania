d0a3c56af1a2d8fc523aef4c592d1dfec9eab176
twarixx <amberful@craftville.net>
2022-11-17 16:32:54 +0100

    Added suggested accounts

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom";
+import users from "./Users";
 
 function Sidebar() {
     return (
@@ -17,10 +18,23 @@ function Sidebar() {
             </div>
             <div
                 className="hidden sm:block w-auto rounded-none sm:rounded-md px-4 py-5 ml-30 w-2/6 bg-white text-black z-20">
-                <h1 className="header">NOOBIE :D</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem doloremque doloribus,
-                    necessitatibus quaerat quidem repellendus? Doloribus, placeat, qui? Consectetur deleniti dolorem
-                    doloribus earum illo molestias quis repellendus sunt tempore voluptatum!</p>
+                <h1 className="header">Suggested Accounts</h1>
+                {/* Get 5 random users without the name Esmaybe*/}
+                {users.filter(user => user.username !== "Esmaybe").sort(() => Math.random() - 0.5).slice(0, 5).map(user => {
+                    return (
+                        <Link to={'/' + user.username} className="flex items-center p-4">
+                            <img className="w-14 aspect-square object-cover h-full rounded-lg"
+                                 src={user.profile_picture || '/images/profile_pictures/_default_.jpg'} alt="Profile Pic"/>
+                            <div className="justify-center ml-3">
+                                <div className="flex items-center">
+                                    <p>{user.display_name}</p>
+                                    {user.verified && <img className="w-5 ml-0 mb-2" src="/icons/verified.svg" title={user.display_name + ' is verified'} alt="Verified"/>}
+                                </div>
+                                <p className="text-gray-400 text-sm">@{user.username}</p>
+                            </div>
+                        </Link>
+                    )
+                })}
             </div>
         </div>
     )
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -4,8 +4,6 @@ const users = [
         display_name: 'Idk',
         verified: false,
         profile_picture: '/images/profile_pictures/Idk.jpg',
-        posts: [],
-
     },
     {
         username: 'admin',
@@ -26,14 +24,26 @@ const users = [
         display_name: 'Shaquille',
         verified: true,
         profile_picture: '/images/profile_pictures/shaquille.jpg',
-        posts: [],
+        posts: [
+            {
+                id: 'ELIE',
+                caption: 'This is elmo!',
+                source: 'https://images.unsplash.com/photo-1581195234513-4dccee3e1898?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80',
+            }
+        ],
     },
     {
         username: 'Esmaybe',
         display_name: 'Esmay',
         verified: true,
         profile_picture: '/images/profile_pictures/esmay.jpg',
-        posts: [],
+        posts: [
+            {
+                id: 'IHATETHIS',
+                caption: 'Hello World',
+                source: 'https://images.unsplash.com/photo-1667387322417-c434fa1f563d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
+            }
+        ],
     }
 ]
 
