55532175c0476ce0bebbc27e632d24928d0dd82b
twarixx <amberful@craftville.net>
2022-11-18 13:53:20 +0100

    Cleaned up feed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,13 +4,13 @@ import Post from "../components/Post";
 function HomePage() {
     return (
         <>
-            <div
-                className="rounded-none relative sm:rounded-md mx-[3px] px-4 py-5 w-full bg-white h-auto text-black z-20">
-                <div>
-                    <h1 className="header">Feed</h1>
-                    <p className="text-sm text-center">Here are the most recent posts by the people you follow.</p>
-                </div>
-            </div>
+            {/*<div*/}
+            {/*    className="rounded-none relative sm:rounded-md mx-[3px] px-4 py-5 w-full bg-white h-auto text-black z-20">*/}
+            {/*    <div>*/}
+            {/*        <h1 className="header">Feed</h1>*/}
+            {/*        <p className="text-sm text-center">Here are the most recent posts by the people you follow.</p>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
 
             {
                 users.filter(user => user.posts && !user.posts.isEmpty).map(user => {
