340f359fc75d9675e0fe629f2257ec0d612dcf16
twarixx <amberful@craftville.net>
2022-11-18 13:51:57 +0100

    Clickable pfp :D

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -13,13 +13,14 @@ function UserPage() {
             <div
                 className="rounded-none relative sm:rounded-md mx-[3px] px-4 py-5 w-full bg-white h-auto text-black z-20">
                 <div className="flex">
-                    <img className="w-36 aspect-square object-cover h-full rounded-md"
-                         src={foundUser.profile_picture || '/images/profile_pictures/_default_.jpg'} alt="Profile Pic"/>
+                    <a target="_blank" href={foundUser.profile_picture || '/images/profile_pictures/_default_.jpg'}><img className="w-36 aspect-square object-cover h-full rounded-md"
+                                                            src={foundUser.profile_picture || '/images/profile_pictures/_default_.jpg'} alt="Profile Pic"/></a>
                     <div className="flex flex-col ml-3">
                         <div className="flex">
                             <p className="text-lg">{foundUser.display_name}</p>
                             {foundUser.verified && <img className="w-5 ml-0 mb-2" src="/icons/verified.svg" title={foundUser.display_name + ' is verified'} alt="Verified"/>}
                         </div>
+
                         <p className="text-gray-400 text-sm">@{foundUser.username}</p>
 
                         <div className="flex flex-col mt-auto">
