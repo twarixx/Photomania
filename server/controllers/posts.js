5485999f61b3944d492691eb8fddaf7ab190dbbd
twarixx <amberful@craftville.net>
2022-11-23 17:53:59 +0100

    Added GET posts endpoint

---
+++ server/controllers/posts.js
@@ -0,0 +1,19 @@
+import {db} from "../database.js";
+import jwt from "jsonwebtoken";
+
+export const getPosts = (req, res) => {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json("You are not logged in.");
+
+    jwt.verify(token, 'shhhhh', (err, userInfo) => {
+        if (err) return res.status(401).json("You are not logged in.");
+
+        const query = "SELECT p.*, u.id as userId, u.username, u.display_name, u.profile_picture FROM social_posts AS p JOIN social_users AS u ON p.posterId = u.id WHERE p.posterId = ? ORDER BY p.timestamp DESC";
+
+        db.query(query, [userInfo.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+
+            return res.status(200).json(data);
+        });
+    });
+};
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
 
 app.use((req, res, next) => {
    res.header("Access-Control-Allow-Credentials", true);
@@ -17,6 +18,7 @@ app.use(cors({
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8500, () => {
     console.log("Server is running on port 8500");
