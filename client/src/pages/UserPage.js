0e17b2a2b08c186fbc0fcb795415ad9e665aea2f
twarixx <amberful@craftville.net>
2022-11-28 07:08:30 +0100

    Added follower count

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -104,7 +104,7 @@ function UserPage() {
                         <p className="text-gray-400 text-sm">@{user.username}</p>
 
                         <div className="flex flex-col mt-auto">
-                            <p><span className="profilecount">0 </span>  {0 ? 'follower' : 'followers'}</p>
+                            <p><span className="profilecount">{following.length} </span>  {following.length ? 'follower' : 'followers'}</p>
                             <p><span className="profilecount">0 </span>  following</p>
                             <p><span className="profilecount">0 </span>  {0 ? 'post' : 'posts'}</p>
                         </div>
