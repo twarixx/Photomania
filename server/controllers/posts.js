0a8d74fb54156105671fee7e0ef1e2e1151951ed
twarixx <amberful@craftville.net>
2022-11-30 17:08:22 +0100

    Fixed posts loading double

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,7 +9,7 @@ export const getPosts = (req, res) => {
     jwt.verify(token, 'shhhhh', (err, userInfo) => {
         if (err) return res.status(401).json("You are not logged in.");
 
-        const query = "SELECT p.*, u.id as userId, u.username, u.display_name, u.profile_picture, u.verified " +
+        const query = "SELECT DISTINCT p.*, u.id as userId, u.username, u.display_name, u.profile_picture, u.verified " +
             "FROM social_posts AS p JOIN social_users AS u ON p.posterId = u.id " +
             "LEFT JOIN social_relationships AS sr on (p.posterId = sr.followed_user_id) " +
             "WHERE sr.follower_user_id = ? OR p.posterId = ? " +
